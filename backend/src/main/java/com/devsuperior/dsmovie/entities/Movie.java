8a902332606730f867e055b6c3b750f3d6efb422
Lucas Tengan <lucastengan@usp.br>
2022-01-15 00:43:49 -0300

    Fixing problem on Lombok

--- backend/src/main/java/com/devsuperior/dsmovie/entities/Movie.java
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Movie.java
@@ -10,9 +10,6 @@ import java.util.Set;
 
 @Entity
 @Table(name = "tb_movie")
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Movie {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,4 +21,64 @@ public class Movie {
 
     @OneToMany(mappedBy = "id.movie")
     private Set<Score> scores = new HashSet<>();
+
+    public Movie() {
+    }
+
+    public Movie(Long id, String title, Double score, Integer count, String image, Set<Score> scores) {
+        this.id = id;
+        this.title = title;
+        this.score = score;
+        this.count = count;
+        this.image = image;
+        this.scores = scores;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Set<Score> getScores() {
+        return scores;
+    }
+
+    public void setScores(Set<Score> scores) {
+        this.scores = scores;
+    }
 }
