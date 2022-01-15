83b8fd300c322288264bd8f1deba27262a1a66b0
Lucas Tengan <lucastengan@usp.br>
2022-01-15 01:12:44 -0300

    First Homolog

--- backend/src/main/java/com/devsuperior/dsmovie/entities/Movie.java
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Movie.java
@@ -1,9 +1,5 @@
 package com.devsuperior.dsmovie.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
